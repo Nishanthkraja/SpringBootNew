243f3137814dfd8980e2283ac87f7a0751006226
NishanthKraja <nishanthr7105@gmail.com>
2025-07-11 15:32:34 +0530

    first_commit

---
+++ src/main/java/com/example/employee_todo_app/EmployeeTodoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.employee_todo_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeTodoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeTodoAppApplication.class, args);
+	}
+
+}
